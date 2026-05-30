925fab8d10a5fad92176256d6955051182a68ce8
sanju1098 <kumar.sanjay101998@gmail.com>
2026-05-30 20:33:50 +0530

    Day 14 - Authenticated Notes Backend (Production Version)

--- DAY 14/src/middleware/error.middleware.js
+++ DAY 14/src/middleware/error.middleware.js
@@ -1,29 +1,7 @@
-const jwt = require("jsonwebtoken");
-const config = require("../config");
-const User = require("../models/user.model");
-const ApiError = require("../utils/ApiError");
-
-const authMiddleware = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      return next(new ApiError(401, "Token missing"));
-    }
-
-    const token = authHeader.split(" ")[1];
-    const decoded = jwt.verify(token, config.jwtSecret);
-    const user = await User.findById(decoded.id).select("-password");
-
-    if (!user) {
-      return next(new ApiError(401, "User not found"));
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    next(new ApiError(401, "Invalid token"));
-  }
+const errorMiddleware = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message,
+  });
 };
-
-module.exports = authMiddleware;
