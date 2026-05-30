042204884b7210767d6e5b4066134cafa3ca6c0d
sanju1098 <kumar.sanjay101998@gmail.com>
2026-05-30 15:49:52 +0530

    Day 13 - Authorization + Security

--- DAY 13/src/middleware/rateLimit.middleware.js
+++ DAY 13/src/middleware/rateLimit.middleware.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
 
-  max: 100,
+  max: 25, // Limit each IP to 25 requests per `window` (here, per 15 minutes)
 
   message: "Too many requests, try later",
 });
